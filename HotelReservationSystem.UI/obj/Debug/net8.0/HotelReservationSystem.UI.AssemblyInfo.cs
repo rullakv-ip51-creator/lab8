c1309f50bda1c7be1029932139c3010001b76e0a
Kyryll Rulla <gibbositys@gmail.com>
2026-05-14 10:08:08 +0300

    1

--- HotelReservationSystem.UI/obj/Debug/net8.0/HotelReservationSystem.UI.AssemblyInfo.cs
+++ HotelReservationSystem.UI/obj/Debug/net8.0/HotelReservationSystem.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationSystem.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6284ade2cb7c720cc5956da5b79f11c0d996f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationSystem.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationSystem.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HotelReservationSystem/obj/Debug/net8.0/HotelReservationSystem.AssemblyInfo.cs
+++ HotelReservationSystem/obj/Debug/net8.0/HotelReservationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6284ade2cb7c720cc5956da5b79f11c0d996f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
